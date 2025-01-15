536d0c034560b7d12f17d9f2a95c03ff1bd0729d
crds83 <119341646+crds83@users.noreply.github.com>
2025-01-15 13:55:25 +0000

    8th

--- src/app/back/page.tsx
+++ src/app/back/page.tsx
@@ -4,9 +4,8 @@ type Greeting = {
   _id: string;
 }
 export default async function Back() {
-  return
-  <div></div>
-  /*const baseUrl = "https://m5-g2.vercel.app/"
+  
+  const baseUrl = "https://m5-g2.vercel.app/"
   const response = await fetch(`${baseUrl}/api`)
   const greetings: Greeting[] = await response.json()
   return (
@@ -15,5 +14,5 @@ export default async function Back() {
         <EditGreeting greetingObj={greetingObj} key={greetingObj._id.toString()}/>
       )}
     </div>
-  )*/
+  )
 }
